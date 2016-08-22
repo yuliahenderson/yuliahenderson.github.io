ca7e8bcea68658b205ab95f00633df8adffdb995
Julia Henderson <yulia.p.henderson@gmail.com>
2016-08-21 22:21:29 -0400

    functions to side-menu and main image

---
+++ script.js
@@ -0,0 +1,21 @@
+// hamburger menu function
+function openNav(){
+  document.getElementById("mySideNav").style.width = "250px";
+}
+function closeNav(){
+  document.getElementById("mySideNav").style.width = "0";
+}
+// hero-image function
+function rollover(main_image){
+  main_image.src="images/henderson_3.png";
+}
+function mouseaway(main_image){
+  main_image.src="images/henderson.png";
+}
+function myFunction1 {
+  window.open("https://github.com/yuliahenderson");
+}
+function myFunction2 {
+  window.open("https://www.linkedin.com/in/julia-henderson-11b9b73b");
+}
+
